686b95eebc3779ef753e7ce26a739e7936e162d4
Cade <cade@shiba.computer>
2018-02-24 20:05:25 +0100

    Move Menu.buildFromTemplate into the module

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 const Electron = require('electron');
 const Application = Electron.app;
 const BrowserWindow = Electron.BrowserWindow;
+const Menu = Electron.Menu;
 const EventEmitter = new (require('events').EventEmitter);
 const FileSystem = require('fs');
 const WatchJS = require('melanke-watchjs');
@@ -196,7 +197,7 @@ Window.prototype.create = function(url) {
     // Set the window menu (null is valid to not have a menu at all)
     if(this.setup.menu !== undefined){
         if(process.platform === 'darwin') {
-          this.object.setApplicationMenu(this.setup.menu);
+          this.object.setApplicationMenu(Menu.buildFromTemplate(this.setup.menu));
         } else {
           this.object.setMenu(this.setup.menu);
         }
